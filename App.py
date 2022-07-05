98f2d977d39c81fcb2a819b472d8bdb05a6deb51
Ilyas Bijediguen <ibijediguen@masaratapp.com>
2022-07-05 11:45:50 +0100

    first commit

---
+++ App.py
@@ -0,0 +1,55 @@
+import base64
+import os
+from flask import Flask,  request,render_template,jsonify,send_file
+from werkzeug.utils import secure_filename
+from brisque import BRISQUE
+
+from time import time
+
+app = Flask(__name__)
+
+
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+def allowed_file(filename):
+	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/')
+def upload_init():
+		return jsonify({'message' : ' ilyas li kayn'})
+
+
+
+
+
+@app.route('/upload', methods=['GET','POST'])
+def upload_submit():
+
+	if 'file' not in request.files:
+		resp = jsonify({'message' : 'No file part in the request'})
+		resp.status_code = 400
+		return resp
+	file = request.files['file']
+	if file.filename == '':
+		resp = jsonify({'message' : 'No file selected for uploading'})
+		resp.status_code = 400
+		return resp
+	if file and allowed_file(file.filename):
+		
+		filename = secure_filename(str(time())+file.filename )
+		file.save(os.path.join("uploads", filename))
+		# quality detection 
+		obj = BRISQUE("uploads/"+filename, url=False)
+		quality  = obj.score()
+
+		# percentage quality -->150 to 120  = 62 to 52 
+		percentage = 100-((quality*100)/120)
+		percentageInt = int(percentage)
+
+		resp = jsonify({"quality": percentageInt})
+		resp.status_code = 201
+		os.remove("uploads/"+filename)
+		return resp
+if __name__ == '__main__':
+    app.run(debug=True)
